3f63b7479ea477c1b1d54ef5f7a5af974229d89f
kutho53 <kurtisthomas53@gmail.com>
2024-10-13 12:57:47 -0400

    created basic fetch for HTML

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const apiUrl = 'https://www.cheapshark.com/api/1.0/deals?pageSize=5';
+
+function fetchData() {
+    fetch(apiUrl)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(element => {
+            const newTitle = document.createElement("p");
+            newTitle.innerText = element.title;
+            document.getElementById('apiResponse').appendChild(newTitle)
+            const img = document.createElement('img');
+            img.src = element.thumb;
+            document.getElementById('apiResponse').appendChild(img)
+            const salePrice = document.createElement("p");
+            salePrice.innerText = element.salePrice;
+            document.getElementById('apiResponse').appendChild(salePrice)
+            const normalPrice = document.createElement("p");
+            normalPrice.innerText = element.normalPrice;
+            document.getElementById('apiResponse').appendChild(normalPrice)
+        });
+    })
+}
