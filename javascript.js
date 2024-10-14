dc8734da302b5b049775e3edfb032da5f7b565cd
kutho53 <kurtisthomas53@gmail.com>
2024-10-13 20:51:55 -0400

    Adeed Search bar functionality.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,39 @@
 const apiUrl = 'https://www.cheapshark.com/api/1.0/deals?pageSize=9';
 
-function fetchData() {
-    fetch(apiUrl)
+//API fetch code.
+function fetchData(api) {
+    fetch(api)
     .then(response => {
         return response.json();
     })
     .then(data => {
+        const apiResponseDiv = document.getElementById('apiResponse')
+        while (apiResponseDiv.firstChild) {
+            apiResponseDiv.removeChild(apiResponseDiv.firstChild);
+        }
         data.forEach(element => {
             const newTitle = document.createElement("p");
             newTitle.innerText = element.title;
-            document.getElementById('apiResponse').appendChild(newTitle)
+            apiResponseDiv.appendChild(newTitle);
+
             const img = document.createElement('img');
             img.src = element.thumb;
-            document.getElementById('apiResponse').appendChild(img)
+            apiResponseDiv.appendChild(img)
+
             const salePrice = document.createElement("p");
             salePrice.innerText = element.salePrice;
-            document.getElementById('apiResponse').appendChild(salePrice)
+            apiResponseDiv.appendChild(salePrice)
+
             const normalPrice = document.createElement("p");
             normalPrice.innerText = element.normalPrice;
-            document.getElementById('apiResponse').appendChild(normalPrice)
+            apiResponseDiv.appendChild(normalPrice)
         });
     })
+}
+
+//Search bar code.
+function fetchSearchData(){
+    const searchInput = document.getElementById('searchInput');
+    const apiUrlSearch = `https://www.cheapshark.com/api/1.0/deals?pageSize=9&title=${searchInput.value}`;
+    fetchData(apiUrlSearch);
 }
